db55f2c27f470d3f7255a148fdba18fa07d3c4da
Upendra Upadhyay <upendra.upadhyay@gojek.com>
2023-11-15 17:37:38 +0530

    removing extra shift and moving layer

--- keyboards/for_science/keymaps/default/keymap.c
+++ keyboards/for_science/keymaps/default/keymap.c
@@ -23,12 +23,10 @@ enum layer_names {
     _FUNCT
 };
 
-#define SFT_A SFT_T(KC_A)
-#define SFT_F1 SFT_T(KC_F1)
 #define SFT_Z SFT_T(KC_Z)
 #define SFT_F6 SFT_T(KC_F6)
-#define LAY_X LT(_LAYER, KC_X)
-#define LAY_F7 LT(_LAYER, KC_F7)
+#define LAY_A LT(_LAYER, KC_A)
+#define LAY_F1 LT(_LAYER, KC_F1)
 #define LAY_SLS LT(_LAYER, KC_SLSH)
 #define LAY_SPC LT(_LAYER, KC_SPACE)
 
@@ -50,10 +48,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-------+-------+-------+-------+-------+   |-------+-------+-------+-------+-------|
  * |   A   |   S   |   D   |   F   |   G   |   |   H   |   J   |   K   |   L   |   ;   |
  * |  F1   |  F2      F3   |  F4   |  F5   |   |  F11  |   🔉  |  Up   |   🔊  | Enter |
- * |-Shift-+-------+-------+-------+-------+   |-------+-------+-------+-------+-------|
+ * |-Layer-+-------+-------+-------+-------+   |-------+-------+-------+-------+-------|
  * |   Z   |   X   |   C   |   V   |   B   |   |   N   |   M   |   ,   |   .   |   /   |
  * |  F6   |  F7   |  F8   |  F9   |  F10  |   |  F12  | Left  | Down  | Right |       |
- * `-Shift---Layer-+-------+-------+-------+   |-------+-------+-------+---------Layer-'
+ * `-Shift---------+-------+-------+-------+   |-------+-------+-------+---------Layer-'
  *                 | Ctrl  |  Gui  |  Alt  |   | Space | Layer | Shift |
  *                 |       |       |       |   | Funct |       |       |
  *                 `------------------------   `-Layer-----------------'
@@ -62,16 +60,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT_split_4x5_3(
     KC_TAB,  KC_ESC,  KC_LPRN, KC_LCBR, KC_LBRC,    KC_BSPC, KC_QUOT, KC_RPRN, KC_RCBR, KC_RBRC,
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_P,    KC_O,    KC_I,    KC_U,    KC_Y,
-    SFT_A,   KC_S,    KC_D,    KC_F,    KC_G,       KC_SCLN, KC_L,    KC_K,    KC_J,    KC_H,
-    SFT_Z,   LAY_X,   KC_C,    KC_V,    KC_B,       LAY_SLS, KC_DOT,  KC_COMM, KC_M,    KC_N,
+    LAY_A,   KC_S,    KC_D,    KC_F,    KC_G,       KC_SCLN, KC_L,    KC_K,    KC_J,    KC_H,
+    SFT_Z,   KC_X,    KC_C,    KC_V,    KC_B,       LAY_SLS, KC_DOT,  KC_COMM, KC_M,    KC_N,
                       KC_LCTL, KC_LGUI, KC_LALT,    KC_RSFT, LAYER,   LAY_SPC
 ),
 
 [_LAYER] = LAYOUT_split_4x5_3(
     KC_TILD, KC_GRV,  KC_LABK, KC_UNDS, KC_MINS,    KC_DEL,  KC_BSLS, KC_RABK, KC_EQL,  KC_PLUS,
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_0,    KC_9,    KC_8,    KC_7,    KC_6,
-    SFT_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_ENT,  KC_VOLU, KC_UP,   KC_VOLD, KC_F11,
-    SFT_F6,  LAY_F7,  KC_F8,   KC_F9,   KC_F10,     _______, KC_RGHT, KC_DOWN, KC_LEFT, KC_F12,
+    LAY_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_ENT,  KC_VOLU, KC_UP,   KC_VOLD, KC_F11,
+    SFT_F6,  KC_F7,   KC_F8,   KC_F9,   KC_F10,     _______, KC_RGHT, KC_DOWN, KC_LEFT, KC_F12,
                       _______, _______, _______,    _______, _______, FUNCT
 ),
 
