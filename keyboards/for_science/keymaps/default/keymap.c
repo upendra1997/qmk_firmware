529f7db66f4a50a2c9aa9097c89b88d0f732e32c
Upendra Upadhyay <upendra.upadhyay@gojek.com>
2023-11-16 14:02:25 +0530

    modifying few symbol keys to make frequent keys easy

--- keyboards/for_science/keymaps/default/keymap.c
+++ keyboards/for_science/keymaps/default/keymap.c
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_DVORAK] = LAYOUT_split_4x5_3(
-    KC_TAB,   KC_ESC,  KC_LPRN, KC_LCBR, KC_LBRC,    KC_BSPC, KC_SLSH, KC_RPRN, KC_RCBR, KC_RBRC,
+    KC_TAB,   KC_ESC,  KC_LPRN, KC_UNDS, KC_MINS,    KC_BSPC, KC_SLSH, KC_RPRN, KC_EQL,  KC_PLUS,
     KC_QUOT,  KC_COMM, KC_DOT,  KC_P,    KC_Y,       KC_L,    KC_R,    KC_C,    KC_G,    KC_F,
     LAY_A,    KC_O,    KC_E,    KC_U,    KC_I,       KC_S,    KC_N,    KC_T,    KC_H,    KC_D,
     SFT_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,       SFT_Z,   KC_V,    KC_W,    KC_M,    KC_B,
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_QWERTY] = LAYOUT_split_4x5_3(
-    KC_TAB,  KC_ESC,  KC_LPRN, KC_LCBR, KC_LBRC,    KC_BSPC, KC_QUOT, KC_RPRN, KC_RCBR, KC_RBRC,
+    KC_TAB,  KC_ESC,  KC_LPRN, KC_UNDS, KC_MINS,    KC_BSPC, KC_SLSH, KC_RPRN, KC_EQL,  KC_PLUS,
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_P,    KC_O,    KC_I,    KC_U,    KC_Y,
     LAY_A,   KC_S,    KC_D,    KC_F,    KC_G,       KC_SCLN, KC_L,    KC_K,    KC_J,    KC_H,
     SFT_Z,   KC_X,    KC_C,    KC_V,    KC_B,       SFT_SLS, KC_DOT,  KC_COMM, KC_M,    KC_N,
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_LAYER] = LAYOUT_split_4x5_3(
-    KC_TILD, KC_GRV,  KC_LABK, KC_UNDS, KC_MINS,    KC_DEL,  KC_BSLS, KC_RABK, KC_EQL,  KC_PLUS,
+    KC_GRV,  KC_GRV,  KC_LABK, KC_LCBR, KC_LBRC,    KC_DEL,  KC_BSLS, KC_RABK, KC_RCBR, KC_RBRC,
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_0,    KC_9,    KC_8,    KC_7,    KC_6,
     LAY_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_ENT,  KC_VOLU, KC_UP,   KC_VOLD, KC_F11,
     SFT_F6,  KC_F7,   KC_F8,   KC_F9,   KC_F10,     _______, KC_RGHT, KC_DOWN, KC_LEFT, KC_F12,
